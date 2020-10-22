ed5d35beef2082b96da556ee3b240b7b06ae7953
sugyeong Kim <s2018s25@e-mirim.hs.kr>
2020-10-22 23:49:19 +0900

    setting

---
+++ src/main/java/com/example/Tomato/TomatoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Tomato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomatoApplication.class, args);
+	}
+
+}
